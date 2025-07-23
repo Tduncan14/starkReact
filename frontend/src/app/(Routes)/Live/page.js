411169f1f016f6b1697cdca73a778fa299a5ed30
tduncan14 <nbatre1@yahoo.com>
2025-07-22 21:13:57 -0500

    keep moving forward

---
+++ frontend/src/app/(Routes)/Live/page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './live.css'
+
+
+const Live = () => {
+
+
+
+
+
+
+
+
+    return (
+        <>
+            <p> this is the live</p>
+
+        </>
+    )
+}
+
+
+
+export default Live
